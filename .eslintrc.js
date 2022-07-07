1d4cc926922a3d561e764739c76d9f38cce0d8fe
haipc_dev <phamcong.hai.90@gmail.com>
2022-07-07 09:14:32 +0700

    config v1

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,18 +3,33 @@ module.exports = {
   env: {
     node: true,
   },
+  plugins: ["@typescript-eslint", "prettier"],
   extends: [
-    "plugin:vue/essential",
+    "plugin:vue/strongly-recommended",
     "eslint:recommended",
     "@vue/typescript/recommended",
-    "plugin:prettier/recommended",
+    "prettier",
   ],
   parserOptions: {
     ecmaVersion: 2020,
+    parser: "@typescript-eslint/parser",
   },
+  parser: "vue-eslint-parser",
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "vue/no-multiple-template-root": "off",
+    "prettier/prettier": [
+      "error",
+      {
+        endOfLine: "auto",
+        semi: false,
+        parser: "flow",
+      },
+      {
+        usePrettierrc: true,
+      },
+    ],
   },
   overrides: [
     {
@@ -27,4 +42,4 @@ module.exports = {
       },
     },
   ],
-};
+}
